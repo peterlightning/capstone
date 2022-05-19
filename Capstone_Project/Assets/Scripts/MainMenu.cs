f0d7880198ac4138b60044198b817280fd8e2a9f
peterlightning <ptrjwllnr@gmail.com>
2022-05-19 13:52:27 -0700

    added project

---
+++ Capstone_Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button gameStartButton;
+    public Button gameQuitButton;
+    public string newGameSceneName;
+    public GameObject loadGameMenu;
+
+    public void Awake()
+    {
+        gameStartButton.onClick.AddListener(NewGame);
+        gameQuitButton.onClick.AddListener(ExitGame);
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Capstone_Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,66 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GAME_START_EVENT = 3212575115U;
+        static const AkUniqueID LAGOON_EVENT = 455124344U;
+        static const AkUniqueID LAGOON_STOP_EVENT = 2900974885U;
+        static const AkUniqueID M2_END_EVENT = 3405469899U;
+        static const AkUniqueID M2_START_EVENT = 3522591708U;
+        static const AkUniqueID M3_START_EVENT = 3022445633U;
+        static const AkUniqueID M4_EVENT = 2359787571U;
+        static const AkUniqueID MENU_EVENT = 2456358317U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STATES
+        {
+            static const AkUniqueID GROUP = 1208172739U;
+
+            namespace STATE
+            {
+                static const AkUniqueID AT_LAGOON_STATE = 1078499911U;
+                static const AkUniqueID M1_GAME_START_STATE = 2294279619U;
+                static const AkUniqueID M2_END = 2304912410U;
+                static const AkUniqueID M2_STATE = 3180947566U;
+                static const AkUniqueID M3_STATE = 3431380599U;
+                static const AkUniqueID M4_STATE = 2600222324U;
+                static const AkUniqueID MENU_STATE = 3941853002U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace STATES
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_01 = 3200950630U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
